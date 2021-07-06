c9242f6287daae01a4bfed8a338f89a5166d47fe
unknown <j.podrucka@gmail.com>
2021-07-06 18:40:19 +0200

    fix

--- src/components/drawer/drawer.js
+++ src/components/drawer/drawer.js
@@ -1,9 +1,5 @@
 import React, { useState } from "react"
-import Drawer from "@material-ui/core/drawer"
-import List from "@material-ui/core/List"
-import ListItem from "@material-ui/core/ListItem"
-import ListItemText from "@material-ui/core/ListItemText"
-import IconButton from "@material-ui/core/IconButton"
+import { Drawer, List, ListItem, ListItemText,  IconButton } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import { Link } from "gatsby"
 
